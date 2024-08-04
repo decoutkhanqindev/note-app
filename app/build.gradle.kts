f6346bb1e0b3c5ac88f71d6b8b414e6e3176f28e
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-04 23:09:40 +0700

    add fragment dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.fragment.ktx)
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
