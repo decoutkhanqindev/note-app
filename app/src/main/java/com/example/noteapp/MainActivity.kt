2fdc05dede106ee8c93f41fe5bfd7bc8c084ac1f
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-05 00:40:05 +0700

    handle to visibility password

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.noteapp
 
 import android.os.Bundle
-import android.text.InputType
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
