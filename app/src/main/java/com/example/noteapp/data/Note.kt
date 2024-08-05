49e93757ce2ca15dff496129ddef6bc0222bddc4
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-05 16:00:49 +0700

    handle note adapter to bind data

--- app/src/main/java/com/example/noteapp/data/Note.kt
+++ app/src/main/java/com/example/noteapp/data/Note.kt
@@ -1,6 +1,7 @@
 package com.example.noteapp.data
 
 data class Note(
+    val id: Int,
     val title: String,
     val description: String
 )
--- app/src/main/java/com/example/noteapp/ui/AllNotesActivity.kt
+++ app/src/main/java/com/example/noteapp/ui/AllNotesActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.noteapp.data.Note
 import com.example.noteapp.databinding.ActivityAllNotesBinding
+import kotlin.random.Random
 
 class AllNotesActivity : AppCompatActivity() {
     private val binding by lazy(LazyThreadSafetyMode.NONE) {
@@ -13,6 +14,7 @@ class AllNotesActivity : AppCompatActivity() {
 
     private val noteAdapter by lazy(LazyThreadSafetyMode.NONE) {
         NoteAdapter()
+//        NoteListAdapter()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,51 +30,67 @@ class AllNotesActivity : AppCompatActivity() {
         noteAdapter.updateNotes(
             listOf(
                 Note(
+                    id = Random.nextInt(1, 30),
                     title = "Youtube script ideas \uD83C\uDF96",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "Datastore Blog Ideas  \uD83E\uDD8B",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "College skit review \uD83C\uDFC4\uD83D\uDE1D",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "Social media blogs  \uD83D\uDCD5",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "Youtube script ideas \uD83C\uDF96",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "Datastore Blog Ideas  \uD83E\uDD8B",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "College skit review \uD83C\uDFC4\uD83D\uDE1D",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "Social media blogs  \uD83D\uDCD5",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "Youtube script ideas \uD83C\uDF96",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "Datastore Blog Ideas  \uD83E\uDD8B",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "College skit review \uD83C\uDFC4\uD83D\uDE1D",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "Social media blogs  \uD83D\uDCD5",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "Youtube script ideas \uD83C\uDF96",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "Datastore Blog Ideas  \uD83E\uDD8B",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "College skit review \uD83C\uDFC4\uD83D\uDE1D",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 ), Note(
+                    id = Random.nextInt(1, 30),
                     title = "Social media blogs  \uD83D\uDCD5",
                     description = "Google Play Protect, regular security updates and control over how your data is shared. We’re dedicated to securing Android’s 2.5 billion+ active devices every day and keeping information private.\n" + "\n" + "Screen readers, speech-to-text and some of the newest ways to experience the world your way.\n" + "\n" + "Choices for work, gaming, 5G streaming and anything else. There are over 24,000 phones and tablets that run on Android globally. So no matter what you’re looking for, there’s something for you."
                 )
