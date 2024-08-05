daf19f114a83ff07e520e3149cc9532e8ff73a60
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-05 14:54:26 +0700

    handle note adapter to bind data

--- app/src/main/java/com/example/noteapp/ui/NoteAdapter.kt
+++ app/src/main/java/com/example/noteapp/ui/NoteAdapter.kt
@@ -1,26 +1,39 @@
 package com.example.noteapp.ui
 
-import android.provider.ContactsContract.CommonDataKinds.Note
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noteapp.databinding.NoteItemLayoutBinding
+import com.example.noteapp.data.Note
 
 class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
     private var notes = emptyList<Note>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
-        TODO("Not yet implemented")
+    fun updateNotes(notes: List<Note>) {
+        this.notes = notes
+        notifyDataSetChanged()
     }
 
-    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder =
+        NoteViewHolder(
+            NoteItemLayoutBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) =
+        holder.bind(notes[position])
 
-    class NoteViewHolder(binding: NoteItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    override fun getItemCount(): Int = notes.size
 
+    class NoteViewHolder(private val binding: NoteItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(note: Note) {
+            binding.run {
+                titleNote.text = note.title
+                titleImg.setImageResource(note.img)
+                descriptionNote.text = note.description
+            }
+        }
     }
 }
