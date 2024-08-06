9ed9965616c3b6c5f64487c2adc37c20617eb693
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-06 15:17:54 +0700

    handle note adapter to bind data

--- app/src/main/java/com/example/noteapp/ui/NoteAdapter.kt
+++ app/src/main/java/com/example/noteapp/ui/NoteAdapter.kt
@@ -3,10 +3,11 @@ package com.example.noteapp.ui
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.noteapp.databinding.NoteItemLayoutBinding
 import com.example.noteapp.data.Note
+import com.example.noteapp.databinding.NoteItemLayoutBinding
+import com.example.noteapp.ui.NoteAdapter.NoteViewHolder
 
-class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+class NoteAdapter : RecyclerView.Adapter<NoteViewHolder>() {
     private var notes = emptyList<Note>()
 
     fun updateNotes(notes: List<Note>) {
@@ -36,3 +37,45 @@ class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
         }
     }
 }
+
+//// new style
+//object NoteDiffCallBack : DiffUtil.ItemCallback<Note>() {
+//    override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean =
+//        oldItem.id == newItem.id
+//
+//    override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean = oldItem == newItem
+//}
+//
+//class NoteListAdapter(
+////    private val onItemClick: (Note) -> Unit
+//) : ListAdapter<Note, NoteListAdapter.NoteViewHolder>(NoteDiffCallBack) { // tinh toan dc su khac biet giua list cu va list moi
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder =
+//        NoteViewHolder(
+//            NoteItemLayoutBinding.inflate(
+//                LayoutInflater.from(parent.context), parent, false
+//            )
+//        )
+//
+//    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) =
+//        holder.bind(getItem(position))
+//
+//    inner class NoteViewHolder(private val binding: NoteItemLayoutBinding) :
+//        RecyclerView.ViewHolder(binding.root) {
+//        init {
+//            binding.root.setOnClickListener {
+//                if (adapterPosition != RecyclerView.NO_POSITION) {
+////                    onItemClick(getItem(adapterPosition))
+//                    getItem(adapterPosition)
+//                }
+//            }
+//        }
+//
+//        fun bind(note: Note) {
+//            binding.run {
+//                titleNote.text = note.title
+//                descriptionNote.text = note.description
+//            }
+//        }
+//    }
+//}
