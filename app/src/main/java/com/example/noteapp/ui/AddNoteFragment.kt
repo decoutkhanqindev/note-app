a140541ee2d6db6de6ea20e9f78f15857d137b3a
MeansKane <phamminhkhangpy2003@gmail.com>
2024-08-07 16:34:56 +0700

    handle appearance of add note fragment

--- app/src/main/java/com/example/noteapp/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/AddNoteFragment.kt
@@ -6,13 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.noteapp.R
+import com.example.noteapp.data.Note
+import com.example.noteapp.databinding.FragmentNoteDetailBinding
+import com.example.noteapp.utils.OnNoteChangeClickListener
+import com.example.noteapp.utils.OnNoteDeleteClickListener
 
 class AddNoteFragment : Fragment() {
+    // View Binding and data
+    private var binding: FragmentNoteDetailBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_note, container, false)
+    ): View {
+        binding = FragmentNoteDetailBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
     }
 }
