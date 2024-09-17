2476ee17485bb011ff70d25cbd22c690b918bf41
MeansKane <phamminhkhangpy2003@gmail.com>
2024-09-17 07:49:36 +0700

    update

--- app/src/main/java/com/example/noteapp/database/NoteDao.kt
+++ app/src/main/java/com/example/noteapp/database/NoteDao.kt
@@ -14,9 +14,6 @@ interface NoteDao {
     @Query("SELECT * FROM notes")
     fun observeAllNotes(): LiveData<List<Note>>
 
-//    @Query("SELECT * FROM notes")
-//    suspend fun getAllNotes(): List<Note>
-
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNote(note: Note)
 
